fab95bae518267b244991c843480fe6cedab3535
Abdullah-AboOun <abdullah.h.oun@gmail.com>
2024-11-28 10:38:06 +0200

    register screen improvements

--- src/main/java/com/example/libraryManagementSystem/RegisterController.java
+++ src/main/java/com/example/libraryManagementSystem/RegisterController.java
@@ -2,12 +2,15 @@ package com.example.libraryManagementSystem;
 
 import javafx.event.ActionEvent;
 
-public class RegisterMenuController {
+public class RegisterController {
     public void registerButtonOnClick(ActionEvent actionEvent) {
 
     }
 
     public void switchToLogin(ActionEvent actionEvent) {
         HelperFunctions.switchScene("login");
+    }
+
+    public void uploadImageButtonClick(ActionEvent actionEvent) {
     }
 }
