82e2ecd6f46cd43db88bf09f8842b5736317b9ea
Abdullah-AboOun <abdullah.h.oun@gmail.com>
2024-12-02 18:44:06 +0200

    progress

--- src/main/java/com/example/libraryManagementSystem/BooksDashboardController.java
+++ src/main/java/com/example/libraryManagementSystem/BooksDashboardController.java
@@ -19,92 +19,76 @@ import java.util.ResourceBundle;
 public class BooksDashboardController implements Initializable {
 
     public ImageView profileImageView;
-    public TextField userNameField;
-    public Label userNameErrorLabel;
-    public TextField fullNameField;
-    public Label fullNameErrorLabel;
-    public PasswordField passwordField;
-    public Label passwordErrorLabel;
-    public PasswordField confirmPasswordField;
-    public Label confirmPasswordErrorLabel;
-    public TextField emailField;
-    public Label emailErrorLabel;
-    public TextField phoneField;
-    public Label phoneErrorLabel;
-    public ComboBox roleComboBox;
-    public Label roleErrorLabel;
     public ImageView smallProfileImageView;
-    public TableView<User> userTableView;
-    public TableColumn<User, String> usernameColumn;
-    public TableColumn<User, String> fullNameColumn;
-    public TableColumn<User, String> emailColumn;
-    public TableColumn<User, String> phoneColumn;
-    public TableColumn<User, Role> roleColumn;
-    public TableColumn<User, String> passwordColumn;
-    public TableColumn<User, ImageView> profileImageColumn;
+
+
+//    public TableView<User> userTableView;
+//    public TableColumn<User, String> usernameColumn;
+//    public TableColumn<User, String> fullNameColumn;
+//    public TableColumn<User, String> emailColumn;
+//    public TableColumn<User, String> phoneColumn;
+//    public TableColumn<User, Role> roleColumn;
+//    public TableColumn<User, String> passwordColumn;
+//    public TableColumn<User, ImageView> profileImageColumn;
     public Label usernameLabel;
     User loggedInUser;
     String imagePath;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        roleComboBox.getItems().addAll("Admin", "User", "Librarian");
+//        roleComboBox.getItems().addAll("Admin", "User", "Librarian");
         loggedInUser = MainApplication.userList.get(MainApplication.loggedInUserIndex);
         String imagePath = loggedInUser.getImagePath();
         profileImageView.setImage(new Image(new File(imagePath).toURI().toString()));
         smallProfileImageView.setImage(new Image(new File(imagePath).toURI().toString()));
         usernameLabel.setText(loggedInUser.getFullName());
-        if (loggedInUser.getRole() == Role.USER) {
-            roleComboBox.setDisable(true);
-        } else if (loggedInUser.getRole() == Role.Librarian) {
-            roleComboBox.getItems().remove("Admin");
+
+//        usernameColumn.setCellValueFactory(cellData ->
+//                new SimpleStringProperty(cellData.getValue().getUserName()));
+//
+//        fullNameColumn.setCellValueFactory(cellData ->
+//                new SimpleStringProperty(cellData.getValue().getFullName()));
+//
+//        emailColumn.setCellValueFactory(cellData ->
+//                new SimpleStringProperty(cellData.getValue().getEmail()));
+//
+//        phoneColumn.setCellValueFactory(cellData ->
+//                new SimpleStringProperty(cellData.getValue().getPhoneNumber()));
+//
+//        roleColumn.setCellValueFactory(cellData ->
+//                new SimpleObjectProperty<>(cellData.getValue().getRole()));
+//
+//        passwordColumn.setCellValueFactory(cellData ->
+//                new SimpleStringProperty(cellData.getValue().getPassword()));
+//
+//        profileImageColumn.setCellValueFactory(cellData -> {
+//            String userImagePath = cellData.getValue().getImagePath();
+//            ImageView imageView = new ImageView(new Image(new File(userImagePath).toURI().toString()));
+//            imageView.setFitHeight(30);
+//            imageView.setFitWidth(30);
+//            return new SimpleObjectProperty<>(imageView);
+//        });
+//
+//        userTableView.setItems(MainApplication.userList);
+//
+//        userTableView.getSelectionModel().selectedItemProperty().addListener((
+//                observableValue, oldValue, newValue) -> {
+//
+//            if (newValue != null) {
+//                User user = userTableView.getSelectionModel().getSelectedItem();
+//                userNameField.setText(user.getUserName());
+//                fullNameField.setText(user.getFullName());
+//                emailField.setText(user.getEmail());
+//                phoneField.setText(user.getPhoneNumber());
+//                roleComboBox.setValue(user.getRole());
+//                passwordField.setText(user.getPassword());
+//                confirmPasswordField.setText(user.getPassword());
+//                profileImageView.setImage(new Image(new File(user.getImagePath()).toURI().toString()));
+//                smallProfileImageView.setImage(new Image(new File(user.getImagePath()).toURI().toString()));
+
+//        });
+
         }
-        usernameColumn.setCellValueFactory(cellData ->
-                new SimpleStringProperty(cellData.getValue().getUserName()));
-
-        fullNameColumn.setCellValueFactory(cellData ->
-                new SimpleStringProperty(cellData.getValue().getFullName()));
-
-        emailColumn.setCellValueFactory(cellData ->
-                new SimpleStringProperty(cellData.getValue().getEmail()));
-
-        phoneColumn.setCellValueFactory(cellData ->
-                new SimpleStringProperty(cellData.getValue().getPhoneNumber()));
-
-        roleColumn.setCellValueFactory(cellData ->
-                new SimpleObjectProperty<>(cellData.getValue().getRole()));
-
-        passwordColumn.setCellValueFactory(cellData ->
-                new SimpleStringProperty(cellData.getValue().getPassword()));
-
-        profileImageColumn.setCellValueFactory(cellData -> {
-            String userImagePath = cellData.getValue().getImagePath();
-            ImageView imageView = new ImageView(new Image(new File(userImagePath).toURI().toString()));
-            imageView.setFitHeight(30);
-            imageView.setFitWidth(30);
-            return new SimpleObjectProperty<>(imageView);
-        });
-
-        userTableView.setItems(MainApplication.userList);
-
-        userTableView.getSelectionModel().selectedItemProperty().addListener((
-                observableValue, oldValue, newValue) -> {
-
-            if (newValue != null) {
-                User user = userTableView.getSelectionModel().getSelectedItem();
-                userNameField.setText(user.getUserName());
-                fullNameField.setText(user.getFullName());
-                emailField.setText(user.getEmail());
-                phoneField.setText(user.getPhoneNumber());
-                roleComboBox.setValue(user.getRole());
-                passwordField.setText(user.getPassword());
-                confirmPasswordField.setText(user.getPassword());
-                profileImageView.setImage(new Image(new File(user.getImagePath()).toURI().toString()));
-                smallProfileImageView.setImage(new Image(new File(user.getImagePath()).toURI().toString()));
-            }
-        });
-
-    }
 
     public void imageViewOnClick(MouseEvent mouseEvent) {
         FileChooser fileChooser = new FileChooser();
@@ -125,67 +109,67 @@ public class BooksDashboardController implements Initializable {
     public void addButtonOnClick(ActionEvent actionEvent) {
 
 
-        Role role = switch (roleComboBox.getSelectionModel().getSelectedItem().toString()) {
-            case "Admin" -> Role.ADMIN;
-            case "Librarian" -> Role.Librarian;
-            default -> Role.USER;
-        };
+//        Role role = switch (roleComboBox.getSelectionModel().getSelectedItem().toString()) {
+//            case "Admin" -> Role.ADMIN;
+//            case "Librarian" -> Role.Librarian;
+//            default -> Role.USER;
+//        };
+//
+//        String userName = userNameField.getText();
+//        String password = passwordField.getText();
+//        String fullName = fullNameField.getText();
+//        String email = emailField.getText();
+//        String phone = phoneField.getText();
+//        imagePath = imagePath == null ? MainApplication.defaultImagePath : imagePath;
+//        User user = new User(userName, password, fullName, role, email, phone, imagePath);
 
-        String userName = userNameField.getText();
-        String password = passwordField.getText();
-        String fullName = fullNameField.getText();
-        String email = emailField.getText();
-        String phone = phoneField.getText();
-        imagePath = imagePath == null ? MainApplication.defaultImagePath : imagePath;
-        User user = new User(userName, password, fullName, role, email, phone, imagePath);
-
-        if (MainApplication.userList.contains(user)) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText(null);
-            alert.setContentText("User already exists!");
-            alert.showAndWait();
-            return;
-        }
-
-        MainApplication.userList.add(user);
+//        if (MainApplication.userList.contains(user)) {
+//            Alert alert = new Alert(Alert.AlertType.ERROR);
+//            alert.setTitle("Error");
+//            alert.setHeaderText(null);
+//            alert.setContentText("User already exists!");
+//            alert.showAndWait();
+//            return;
+//        }
+//
+//        MainApplication.userList.add(user);
     }
 
     public void updateButtonOnClick(ActionEvent actionEvent) {
 
 
-        String userName = userNameField.getText();
-        String password = passwordField.getText();
-        String fullName = fullNameField.getText();
-        String email = emailField.getText();
-        String phone = phoneField.getText();
-        imagePath = imagePath == null ? MainApplication.defaultImagePath : imagePath;
-        Role role = switch (roleComboBox.getSelectionModel().getSelectedItem().toString()) {
-            case "Admin" -> Role.ADMIN;
-            case "Librarian" -> Role.Librarian;
-            default -> Role.USER;
-        };
-        User user = new User(userName, password, fullName, role, email, phone, imagePath);
-
-        if (!MainApplication.userList.contains(user)) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText(null);
-            alert.setContentText("No user found with user name: " + userName);
-            alert.showAndWait();
-            return;
-        }
-        int i = MainApplication.userList.indexOf(user);
-        MainApplication.userList.set(i, user);
-        userTableView.refresh();
+//        String userName = userNameField.getText();
+//        String password = passwordField.getText();
+//        String fullName = fullNameField.getText();
+//        String email = emailField.getText();
+//        String phone = phoneField.getText();
+//        imagePath = imagePath == null ? MainApplication.defaultImagePath : imagePath;
+//        Role role = switch (roleComboBox.getSelectionModel().getSelectedItem().toString()) {
+//            case "Admin" -> Role.ADMIN;
+//            case "Librarian" -> Role.Librarian;
+//            default -> Role.USER;
+//        };
+//        User user = new User(userName, password, fullName, role, email, phone, imagePath);
+//
+//        if (!MainApplication.userList.contains(user)) {
+//            Alert alert = new Alert(Alert.AlertType.ERROR);
+//            alert.setTitle("Error");
+//            alert.setHeaderText(null);
+//            alert.setContentText("No user found with user name: " + userName);
+//            alert.showAndWait();
+//            return;
+//        }
+//        int i = MainApplication.userList.indexOf(user);
+//        MainApplication.userList.set(i, user);
+//        userTableView.refresh();
     }
 
     public void deleteButtonOnClick(ActionEvent actionEvent) {
-        User user = userTableView.getSelectionModel().getSelectedItem();
-        if (user == null) {
-            return;
-        }
-        MainApplication.userList.remove(user);
+//        User user = userTableView.getSelectionModel().getSelectedItem();
+//        if (user == null) {
+//            return;
+//        }
+//        MainApplication.userList.remove(user);
     }
 
     public void cancelButtonOnClick(ActionEvent actionEvent) {
